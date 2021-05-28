628ae9e6f1085443da52e9f38a8ba4922d60793b
zarrine905 <83436425+zarrine905@users.noreply.github.com>
2021-05-28 22:52:22 +0530

    Update merge_using_pandas.py

--- merge_using_pandas.py
+++ merge_using_pandas.py
@@ -30,17 +30,4 @@ writer.save()
 
 
 
-"""
-import pandas as pd 
-import numpy as np
 
-df1 = pd.DataFrame({'fruit': ['apple', 'banana', 'orange'] * 3,
-                    'weight': ['high', 'medium', 'low'] * 3,
-                    'price': np.random.randint(0, 15, 9)})
-
-df2 = pd.DataFrame({'pazham': ['apple', 'orange', 'pine'] * 2,
-                    'kilo': ['high', 'low'] * 3,
-                    'price': np.random.randint(0, 15, 6)})
-out = df1.merge(df2,left_on=('fruit','weight'),right_on=('pazham','kilo'),how='inner',suffixes=('_left','_right')).head(10)
-print(out)
-"""
